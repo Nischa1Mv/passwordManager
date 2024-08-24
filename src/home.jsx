7afb7da2f5a4c307cb069b7aa751fcc251a700a9
Nischal <sainischalmv@gmail.com>
2024-08-24 23:48:56 +0530

    ADDED loading and the home page wont load if use is not logged in

--- src/home.jsx
+++ src/home.jsx
@@ -1,21 +1,40 @@
 import SearchBar from "./SearchBar";
-
 import { useNavigate } from "react-router-dom";
 import { auth } from "./firebase";
 import { useEffect, useState } from "react";
 import Main from "./Main";
+import Loading from "./loading";
 
 function Home() {
-  const [isUser, setIsUser] = useState(auth.currentUser);
-  useEffect;
+  const navigate = useNavigate();
+  const [isUser, setIsUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsUser(user);
+      } else {
+        navigate("/");
+      }
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, [navigate]);
 
   const logOut = () => {
     auth.signOut().then(() => {
       console.log("logged out");
-      setIsUser(auth.currentUser);
+      setIsUser(null);
     });
   };
-  const navigate = useNavigate();
+  if (loading) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  }
+
   return (
     <>
       <div className=" text-white  w-full h-screen flex flex-col  pt-10 items-center gap-4 relative px-4 py-4">
---
+++ src/loading.jsx
@@ -0,0 +1,12 @@
+function Loading() {
+  return (
+    <>
+      {" "}
+      <div className="w-full h-screen flex items-center justify-center">
+        <div className="text-3xl text-white font-semibold">Loading...</div>
+      </div>
+    </>
+  );
+}
+
+export default Loading;
