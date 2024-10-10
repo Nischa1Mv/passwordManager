ea199a279ccaeebc524f7f8969ecb4eb15b73d52
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-10-10 22:50:37 +0530

    this is the structure of the data stored

---
+++ structure.sql
@@ -0,0 +1,14 @@
+
+users
+ └── {userId}                         
+      └── platforms                    
+           └── {platform}  
+                └── {accounts}         
+                     └──  {userName1}          
+                          ├── email: "user@example.com" 
+                          ├── password: "encryptedPassword123"
+                          └── userName: "userName"
+                     └── {userName2}     
+                          ├── email: "user@example.com"
+                          ├── password: "encryptedPassword123"
+                          └── userName: "userName"
